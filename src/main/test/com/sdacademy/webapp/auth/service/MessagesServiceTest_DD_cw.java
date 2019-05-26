68f8aeba742a450004aea20ca924cb69ff7126e8
Dariusz.Żulpo <dd2000@wp.pl>
2019-05-26 15:38:24 +0200

    26 maja 2019 niedziela ćwiczenie_9 slajd 89 1. uruchom klase WebappAplication 2. w przeglądarce chrom localhost:8080 - wg opisu w komentzrzu

--- src/main/test/com/sdacademy/webapp/auth/service/MessagesServiceTest_DD_cw.java
+++ src/main/test/com/sdacademy/webapp/auth/service/MessagesServiceTest_DD_cw.java
@@ -1,7 +1,55 @@
 package com.sdacademy.webapp.auth.service;
 
-import static org.junit.Assert.*;
+import com.sdacademy.webapp.auth.model.Message;
+import com.sdacademy.webapp.auth.repository.MessagesRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MessagesServiceTest_DD_cw {
+
+    @Mock
+    private MessageSender messageSender;
+
+    @Mock
+    private MessagesRepository messagesRepository;
+
+    @Mock
+    private MessagesIdGenerator messagesIdGenerator;
+
+    @InjectMocks
+    private MessagesService messagesService;
+
+    @Captor
+    private ArgumentCaptor<Collection<Message>> captor;
+
+    @Test
+    public void shouldSaveAndSend(){   // w JUnit4 musi być PUBLIC
+        final String topic = "topic1";
+        final String messageValue = "wiadomość1";
+        final long timestamp = 123L;
+
+        when(messagesIdGenerator.generate()).thenReturn(timestamp);
+
+        messagesService.saveAndSend(topic,messageValue);
+
+        verify(messagesRepository)
+                .save(new Message(timestamp, topic, messageValue));
+        verify(messageSender).sendMessages(captor.capture());
+        assertTrue(captor.getValue()
+        .contains(new Message(timestamp, topic, messageValue)));
+
+    }
 
 }
