6c425b6051ad018139f541ccbdaa382393891bb2
Dariusz.Żulpo <dd2000@wp.pl>
2019-05-26 15:12:49 +0200

    26 maja 2019 niedziela ćwiczenie_9 slajd 89 1. uruchom klase WebappAplication 2. w przeglądarce chrom localhost:8080 - wg opisu w komentzrzu

--- src/main/java/com/sdacademy/webapp/WebappApplication.java
+++ src/main/java/com/sdacademy/webapp/WebappApplication.java
@@ -9,6 +9,13 @@ public class WebappApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
     }
+    // jdbc:h2:mem:testdb
+    // user: sa    bez hasła
+    // w przeglądarce:
+    // http://localhost:8080/messages/temat1/wiadomosc1_wiadomosc2_wiadomosc3
+    // wyśle wiadomość; podkreślniki oddzielają kolejne wiadomości
+    // http://localhost:8080/messages/remove/temat3/
+    // usunie wszystkie wiadomości z tematu3
 
 }
 
